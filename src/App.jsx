4cea7e7500ae8c7a4ed1990bb957025e2c0d94eb
Guilherme Reis <guireis1164@gmail.com>
2025-05-02 10:40:37 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// import Header from "./Components/Header/index.jsx"
+// import Footer from "./Components/Footer/index.jsx"
+
+import GameLibrary from "./pages/GameLibrary/index.jsx"
+
+export default function App() {
+  return (
+    <>
+    <main>
+      <GameLibrary />
+    </main>
+    </>
+  )
+}
---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import "./style/reset.css"
+import "./style/globals.css"
+
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
+
+import App from "./App.jsx"
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+)
